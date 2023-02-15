26e9487370ada379703973fcaeae0e2ac02d7d3b
HypCarlos <66544442+HypCarlos@users.noreply.github.com>
2023-02-15 13:27:12 -0500

    Time: 5 ms (62.09%), Space: 43 MB (26.42%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -6,16 +6,16 @@ class Solution {
         int[] results = new int[2];  // ARRAY OF 2 FOR RESULT
         int remainder = 0;
         
-        for (int i = 0; i < nums.length; i++) {
+        for(int i = 0; i < nums.length; i++) {
             remainder = target - nums[i];
             
             if (hash.containsKey(remainder)) {
                 results[0] = i;
                 results[1] = hash.get(remainder);
             }
-            
-            hash.put(nums[i],i);
-        }
+                hash.put(nums[i], i);
+                }
         return results;
     }
-}
+                
+    }
